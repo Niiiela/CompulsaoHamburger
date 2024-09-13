49df79059b1295288330e70f4959d4f62fd8215e
niiiela <danielavieira2024@gmail.com>
2024-09-13 06:32:26 -0300

    Imagens

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -20,7 +20,7 @@
         <div class="row align-items-center">
             <div class="col p-2">
                 <!--Begin::Logo-->
-                <a href="<?= site_url('/')?>"><img class="logo" src="<?= base_url('assets/images/logo.png') ?>" alt="Logo" /></a>
+                <a href="<?= site_url('/')?>"><img class="logo" src="<?= base_url('assets/images/LogoCompulsao_hamburger.png') ?>" alt="Logo" /></a>
                 <!--End::Logo-->
             </div>
             <div class="col p-3 pe-5 d-flex flex-row justify-content-end">
