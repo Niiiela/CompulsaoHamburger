55534cce734d8cfcf27749b2eeb9cb6cf1bc706a
niiiela <danielavieira2024@gmail.com>
2024-09-22 10:57:52 -0300

    .

---
+++ app/Controllers/Products.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Products extends BaseController
+{
+    public function index()
+    {
+        // index
+        return view ('products/index');
+    }
+
+}
