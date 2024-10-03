ae729552e0410f51b86736ffb4060966af0476aa
niiiela <danielavieira2024@gmail.com>
2024-10-03 06:38:06 -0300

    .

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -3,13 +3,13 @@
     <div class="row justify-content-center">
         <div class="col-6 d-flex flex-row justify-content-center">
             <div class="text-center mx-4">
-                <a href="#"><img class="logo-flooter" src="<?= base_url('assets/img/facebook.png') ?>" alt="Facebook"></a>
+                <a href="#"><img class="logo-flooter" src="<?= base_url('assets/img/footer/facebook.png') ?>" alt="Facebook"></a>
             </div>
             <div class="text-center mx-4">
-                <a href="#"><img class="logo-flooter" src="<?= base_url('assets/img/instagram.png') ?>" alt="Instagram"></a>
+                <a href="#"><img class="logo-flooter" src="<?= base_url('assets/img/footer/instagram.png') ?>" alt="Instagram"></a>
             </div>
             <div class="text-center mx-4">
-                <a href="#"><img class="logo-flooter" src="<?= base_url('assets/img/whatsapp.png') ?>" alt="Whatsapp"></a>
+                <a href="#"><img class="logo-flooter" src="<?= base_url('assets/img/footer/whatsapp.png') ?>" alt="Whatsapp"></a>
             </div>
         </div>
     </div>
